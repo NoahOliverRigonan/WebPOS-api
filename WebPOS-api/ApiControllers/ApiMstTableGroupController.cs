2b449e5278ea76d2b5aec9b47b0b35020d39533b
mahilumphil <phil.yupte@gmail.com>
2017-05-10 18:02:31 -0700

    MstTableGroup api , entities

---
+++ WebPOS-api/ApiControllers/ApiMstTableGroupController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebPOS_api.ApiControllers
+{
+    //[Authorize]
+    [RoutePrefix("api/tableGroup")]
+    public class ApiMstTableGroupController : ApiController
+    {
+        private Data.posdbDataContext db = new Data.posdbDataContext();
+
+        [HttpGet, Route("list")]
+        public List<Entities.MstTableGroup> listMstTableGroupId()
+        {
+            var tableGroup = from d in db.MstTableGroups
+                             where d.TableGroup != "Delivery" && d.TableGroup != "Walk-in"
+                             select new Entities.MstTableGroup
+                             {
+                                 Id = d.Id,
+                                 EntryUserId = d.EntryUserId,
+                                 EntryDateTime = d.EntryDateTime,
+                                 TableGroup = d.TableGroup,
+                                 UpdateDateTime = d.UpdateDateTime,
+                                 UpdateUserId = d.UpdateUserId,
+                                 IsLocked = d.IsLocked
+                             };
+            return tableGroup.ToList();
+        }
+    }
+}
