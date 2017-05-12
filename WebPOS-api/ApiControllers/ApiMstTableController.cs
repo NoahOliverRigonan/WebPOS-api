9877c6b1daef4e27df07da8ebdaecbab8ff3f72d
mahilumphil <phil.yupte@gmail.com>
2017-05-11 17:10:50 -0700

    commit: MstTableApi

---
+++ WebPOS-api/ApiControllers/ApiMstTableController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebPOS_api.ApiControllers
+{
+
+    [RoutePrefix("api/table")]
+    public class ApiMstTableController : ApiController
+    {
+        private Data.posdbDataContext db = new Data.posdbDataContext();
+
+        [HttpGet, Route("list/{tableId}")]
+        public List<Entities.MstTable> listMstTableId(String tableId)
+        {
+            var table = from d in db.MstTables
+                        where d.TableGroupId == Convert.ToInt32(tableId)
+                        select new Entities.MstTable
+                        {
+                            Id = d.Id,
+                            TableCode = d.TableCode,
+                            TableGroupId = d.TableGroupId,
+                            TopLocation = d.TopLocation,
+                            LeftLocation = d.LeftLocation
+                        };
+
+            return table.ToList();
+        }
+    }
+}
